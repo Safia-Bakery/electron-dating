dd7b8646fb0696b4fc429b05308e5849705793f8
Nurmukhammad <sobirovn707@gmail.com>
2024-03-15 15:19:35 +0500

    dating

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,8 @@
 import { app, BrowserWindow, ipcMain, IpcMainEvent, Menu, WebContentsPrintOptions } from 'electron'
 import path, { join } from 'node:path'
-import { Options, PythonShell } from 'python-shell'
 // import icon from '../../resources/icon.png'
+import { spawn } from 'child_process'
+import fs from 'fs'
 
 const createWindow = (): void => {
   // Create the browser window.
@@ -25,27 +26,50 @@ const createWindow = (): void => {
   })
 
   ipcMain.on('ipc-print', async (_, options: WebContentsPrintOptions) => {
-    const pyoptions: Options = {
-      mode: 'text',
-      pythonPath: 'python', // Path to your Python interpreter
-      pythonOptions: ['-u'], // unbuffered stdout
-      args: [JSON.stringify(options)] // Pass parameters as arguments
-    }
+    // Extract device name and other necessary parameters from options
+    const deviceName = options.deviceName
+    const numCopies = options.copies || 1 // Default to 1 copy if not provided
 
-    const pythonScriptPath = path.join(__dirname, '..', 'main', 'print.py')
+    // Modify the ZPL code with device name and other parameters
+    const zplCode = `
+    ^XA 
+    ^MMT
+    ^PW400
+    ^LL800
+    ^LS0 
+    ^FO50,20
+    ^A0N,50,50
+    ^FDProduct ${deviceName}^FS 
+    ^FO80,30
+    ^BQN,2,9
+    ^FDMM,AWhats up mother fuckers^FS
+    ^FO50,250
+    ^A0N,35,35
+    ^FDPrice: $19.99^FS 
+    ^XZ
+  `
 
-    PythonShell.run(pythonScriptPath, pyoptions).then((messages) => {
-      // results is an array consisting of messages collected during execution
-      console.log('results: %j', messages)
+    // Create a temporary file to hold the ZPL code
+    const tempFile = path.join(__dirname, 'temp_zpl_code.zpl')
+    fs.writeFileSync(tempFile, zplCode)
+
+    // Use subprocess to execute PowerShell command to print
+    const powershell = spawn('powershell', [
+      '-Command',
+      `Start-Process -FilePath "${tempFile}" -Verb Print -PassThru | %{sleep 10;$_} | Stop-Process -Force`
+    ])
+
+    powershell.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`)
     })
-    // mainWindow.webContents.print(
-    //   {
-    //     ...options
-    //   },
-    //   (isSuccess, error) => {
-    //     if (!isSuccess && error) alert(error)
-    //   }
-    // )
+
+    powershell.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`)
+    })
+
+    powershell.on('close', (code) => {
+      console.log(`child process exited with code ${code}`)
+    })
   })
 
   if (process.env['ELECTRON_RENDERER_URL']) {
