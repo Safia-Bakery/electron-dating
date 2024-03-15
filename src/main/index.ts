b4ebd9b8285e77b4bfc4de622fed88d41a3759c0
Nurmukhammad <sobirovn707@gmail.com>
2024-03-15 15:37:15 +0500

    dating

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,36 +1,32 @@
-import { app, BrowserWindow, ipcMain, IpcMainEvent, Menu, WebContentsPrintOptions } from 'electron'
-import path, { join } from 'node:path'
-import { exec } from 'child_process'
-import { promisify } from 'util'
+const { app, BrowserWindow, ipcMain, Menu } = require('electron')
+const path = require('path')
+const { exec } = require('child_process')
+const { promisify } = require('util')
 
-// import icon from '../../resources/icon.png'
-import { spawn } from 'child_process'
-import fs from 'fs'
+// Promisify exec
+const execAsync = promisify(exec)
 
-const createWindow = (): void => {
+function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     height: 600,
     width: 800,
     fullscreenable: true,
     simpleFullscreen: true,
-    // icon,
+    // icon: path.join(__dirname, '../../resources/icon.png'),
     webPreferences: {
-      preload: join(__dirname, '../preload/index.js'),
+      preload: path.join(__dirname, '../preload/index.js'),
       contextIsolation: true
     }
   })
   mainWindow.maximize()
-  // mainWindow.webContents.openDevTools()
 
-  ipcMain.on('get-printers', async (event: IpcMainEvent) => {
+  ipcMain.on('get-printers', async (event) => {
     const printers = await mainWindow.webContents.getPrintersAsync()
     event.reply('get-printers', printers)
   })
 
-  ipcMain.on('ipc-print', async (_, options: WebContentsPrintOptions) => {
-    const execAsync = promisify(exec)
-
+  ipcMain.on('ipc-print', async (_, options) => {
     async function printZPL(printerName, zplCode, numCopies) {
       try {
         // Open the printer
@@ -52,36 +48,36 @@ const createWindow = (): void => {
 
     // Modify the ZPL code to include device name
     const zplCode = `
-^XA 
-^MMT
-^PW400
-^LL800  // Increased label length to 800 dots
-^LS0 
-^FO50,20  // Moved the starting position down to 150 dots
-^A0N,50,50  // Increased font size to 60 dots
-^FDProduct ^FS 
-^FO80,30  // Moved down to 250 dots
-^BQN,2,9  // QR code command. Format 2 specifies QR code, and the data is encoded as ASCII
-^FDMM,AWhats up mother fuckers^FS  // MM: Mode - A: Alphanumeric. You can adjust the mode based on your data type.
-^FO50,250  // Moved down to 450 dots
-^A0N,35,35  // Increased font size to 40 dots
-^FDPrice: $19.99^FS 
-^XZ
-`
+      ^XA 
+      ^MMT
+      ^PW400
+      ^LL800  // Increased label length to 800 dots
+      ^LS0 
+      ^FO50,20  // Moved the starting position down to 150 dots
+      ^A0N,50,50  // Increased font size to 60 dots
+      ^FDProduct ^FS 
+      ^FO80,30  // Moved down to 250 dots
+      ^BQN,2,9  // QR code command. Format 2 specifies QR code, and the data is encoded as ASCII
+      ^FDMM,AWhats up mother fuckers^FS  // MM: Mode - A: Alphanumeric. You can adjust the mode based on your data type.
+      ^FO50,250  // Moved down to 450 dots
+      ^A0N,35,35  // Increased font size to 40 dots
+      ^FDPrice: $19.99^FS 
+      ^XZ
+    `
 
     // Get your Godex printer's EXACT name from Windows settings
     const printerName = 'Godex G530' // Replace with the actual name
 
     // Number of copies to print
-    const numCopies = 5
+    const numCopies = 1
 
     printZPL(printerName, zplCode, numCopies)
   })
 
-  if (process.env['ELECTRON_RENDERER_URL']) {
-    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
+  if (process.env.ELECTRON_RENDERER_URL) {
+    mainWindow.loadURL(process.env.ELECTRON_RENDERER_URL)
   } else {
-    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
+    mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'))
   }
 
   mainWindow.webContents.on('context-menu', () => {
@@ -96,14 +92,8 @@ const createWindow = (): void => {
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
@@ -111,12 +101,7 @@ app.on('window-all-closed', () => {
 })
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow()
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
